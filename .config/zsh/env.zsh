58731bcf6be4496727eaefa45e2e49f5623e2d0e
Schroedingersdoraemon <1181822466@qq.com>
2020-12-21 13:46:13 +0800

    added flutter bin path

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -7,6 +7,7 @@ export LD_LIBRARY_PATH="/usr/lib:$LD_LIBRARY_PATH"
 
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/bin:$PATH"
+export PATH="$PATH:/home/dylan/prog/flutter/bin"
 
 export EDITOR="nvim"
 export WEB_BROWSER="firefox"
